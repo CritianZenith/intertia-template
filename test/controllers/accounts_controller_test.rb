e0cfd6e4cc28e0eb7068c435b2473fd8d47f4a7f
Jaryd Krishnan <jaryd@hey.com>
2025-03-30 21:36:34 -0500

    Update tests to reflect deleted routes

--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -8,11 +8,6 @@ class AccountsControllerTest < ActionDispatch::IntegrationTest
     sign_in_as(@user)
   end
 
-  test "should get index" do
-    get accounts_url
-    assert_response :success
-  end
-
   test "should get new" do
     get new_account_url
     assert_response :success
@@ -23,29 +18,6 @@ class AccountsControllerTest < ActionDispatch::IntegrationTest
       post accounts_url, params: { account: { name: @account.name } }
     end
 
-    assert_redirected_to account_url(Account.last)
-  end
-
-  test "should show account" do
-    get account_url(@account)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_account_url(@account)
-    assert_response :success
-  end
-
-  test "should update account" do
-    patch account_url(@account), params: { account: { name: @account.name } }
-    assert_redirected_to account_url(@account)
-  end
-
-  test "should destroy account" do
-    assert_difference("Account.count", -1) do
-      delete account_url(@account)
-    end
-
-    assert_redirected_to accounts_url
+    assert_redirected_to settings_url
   end
 end
